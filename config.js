675381b51da07107398964d098eda090667ece8c
Gaspard Beernaert <gaspard@gmail.com>
2016-08-04 12:14:18 +0200

    env defaults to app.json configuration
    for dev purpose, use as fallback the default values that can be edited directly in the app.json file

--- config.js
+++ config.js
@@ -1,15 +1,17 @@
+var defaultEnv = require('./app.json').env;
+
 // Define app configuration in a single location, but pull in values from
 // system environment variables (so we don't check them in to source control!)
 module.exports = {
     // Twilio Account SID - found on your dashboard
-    accountSid: process.env.TWILIO_ACCOUNT_SID,
+    accountSid: process.env.TWILIO_ACCOUNT_SID || defaultEnv.TWILIO_ACCOUNT_SID.value,
 
     // Twilio Auth Token - found on your dashboard
-    authToken: process.env.TWILIO_AUTH_TOKEN,
+    authToken: process.env.TWILIO_AUTH_TOKEN || defaultEnv.TWILIO_AUTH_TOKEN.value,
 
     // A Twilio number that you have purchased through the twilio.com web
     // interface or API
-    twilioNumber: process.env.TWILIO_NUMBER,
+    twilioNumber: process.env.TWILIO_NUMBER || defaultEnv.TWILIO_NUMBER.value,
 
     // The port your web application will run on
     port: process.env.PORT || 3000
